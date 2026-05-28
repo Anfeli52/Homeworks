38636cdc8f733c6ade793e67efc8c5418c532d13
Anfeli52 <anfeli201111@gmail.com>
2026-05-27 21:09:31 -0500

    Chore: Remove accidental file uploaded

--- 11-Challenge/src/models/Product.ts
+++
@@ -1,9 +0,0 @@
-export class Product {
-    name: string;
-    popularity: number;
-
-    constructor(name: string, popularity: number) {
-        this.name = name;
-        this.popularity = popularity;
-    }
-}
